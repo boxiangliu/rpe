bb4e05d50346a66709ab6dba814c0b1c5b57ac9b
Bosh Liu <jollier.liu@gmail.com>
2016-12-11 14:06:09 -0800

    new file:   .gitignore 	modified:   gwas_atacseq_overlap/overlap.gwas_thresholding.R

--- gwas_atacseq_overlap/overlap.gwas_thresholding.R
+++ gwas_atacseq_overlap/overlap.gwas_thresholding.R
@@ -58,16 +58,15 @@ tissues=c('rpe','HRPEpiC',str_match(DNase_samples,'E[0-9]{3}'))
 names(atacseq)=tissues
 
 # overlap summary: 
-# thd=c(1e-3,1e-4,1e-5,1e-6,1e-7)
-thd=c(1e-3)
+thd=c(1e-3,1e-4,1e-5,1e-6,1e-7)
 baseline=nrow(atacseq[['rpe']])
 overlap_summary=data.frame()
 for (this_thd in thd){
 	# thresholding gwas by pvalue: 
 	gwas_thd=gwas[pval<this_thd,]
 
-	for (this_tissue in names(atacseq)[1:2]){
-	# for (this_tissue in names(atacseq)){
+
+	for (this_tissue in names(atacseq)){
 		# get atacseq data:
 		atac=atacseq[[this_tissue]]
 
@@ -93,24 +92,12 @@ for (this_thd in thd){
 
 # calculating fractions:
 overlap_summary=as.data.table(overlap_summary)
-# overlap_summary[,fraction_uniq:=n_unique_peaks/n_peaks]
-# overlap_summary[,fraction:=n_overlaps/n_peaks]
 overlap_summary[,fraction_adj:=fraction/ratio]
-overlap_summary
-
-# calculating lfc:
-overlap_summary[,rank:=rank(fraction_uniq),by='threshold']
-overlap_summary=overlap_summary%>%arrange(threshold,desc(rank))
-lfc=log10(overlap_summary[rank==max(rank),fraction_uniq]/overlap_summary[rank==max(rank)-1,fraction_uniq])
-annotation=data.frame(overlap_summary[rank==max(rank),.(threshold,fraction_uniq,tissue)],lfc=prettyNum(lfc,digits=3))
 
 
 # plotting: 
-p=ggplot(overlap_summary,aes(x=threshold,y=fraction_uniq,color=tissue,size=ifelse(tissue=='HRPEpiC',2,1)))+geom_point()+scale_x_log10(breaks=10^(-seq(3,7)))+scale_y_log10(breaks=10^(-seq(2,4,by=0.3)))+theme_bw()+xlab('Threshold')+ylab('Fraction of unique overlaps')
-p=ggplot(overlap_summary,aes(x=threshold,y=fraction_adj*100,color=tissue,size=ifelse(tissue=='HRPEpiC',2,1)))+geom_point()+scale_x_log10(breaks=10^(-seq(3,7)))+scale_y_log10(breaks=10^(-seq(2,4,by=0.3)))+theme_bw()+xlab('Threshold')+ylab('Fraction of unique overlaps')
-p
+p=ggplot(overlap_summary,aes(x=threshold,y=fraction_adj*100,color=tissue,alpha=ifelse(tissue=='rpe'|tissue=='HRPEpiC',1,0.9)))+geom_line()+scale_x_log10(breaks=10^(-seq(3,7)))+ scale_alpha_continuous(guide = 'none')+theme_bw()+xlab('Threshold')+ylab('Percentage of unique overlaps')
 save_plot('../figures/gwas_atacseq_overlap/gwas_atacseq_overlap.unique.pdf',p,base_height=6,base_width=6)
 
-p1=ggplot(overlap_summary[threshold==1e-7,],aes(x=reorder(tissue,-fraction_uniq,FUN=mean),y=fraction_uniq,color=tissue))+geom_point()+scale_y_log10(breaks=10^(-seq(2,4,by=0.3)))+theme_bw()+xlab('Tissue')+ylab('Fraction of unique overlaps')+theme(axis.text.x=element_text(angle=45,hjust=1))
+p1=ggplot(overlap_summary[threshold==1e-7,],aes(x=reorder(tissue,-fraction_adj,FUN=mean),y=fraction_adj*100,color=tissue))+geom_point()+scale_y_log10(breaks=10^(-seq(2,4,by=0.3)))+theme_bw()+scale_color_discrete(guide='none')+xlab('Tissue')+ylab('Percentage of unique overlaps')+theme(axis.text.x=element_text(angle=45,hjust=1))
 save_plot('../figures/gwas_atacseq_overlap/gwas_atacseq_overlap.unique.1e-7.pdf',p1,base_height=6,base_width=8)
-
